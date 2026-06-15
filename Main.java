4818135922c7c4890112523bff92222b2596416d
GeethapriyanS <geethapriyan.s2023cse@sece.ac.in>
2026-06-15 15:38:19 +0530

    Hello Code added

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("GEETHAPRIYAN S");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Hello, " + name + "!");
+        sc.close();
     }
 }
