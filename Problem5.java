5d1c7948561c672a423d2130124656a52ae03293
GeethapriyanS <geethapriyan.s2023cse@sece.ac.in>
2026-06-15 16:08:20 +0530

    Probelm 5 added

---
+++ Problem5.java
@@ -0,0 +1,9 @@
+public class Problem5 {
+    public static void main(String[] args) {
+        int decimalNumber = 5;
+        String binaryNumber = Integer.toBinaryString(decimalNumber);
+
+        System.out.println("Input a Decimal Number : " + decimalNumber);
+        System.out.println("Binary number is: " + binaryNumber);
+    }
+}
