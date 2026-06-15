ff40e6b9dabbc86d17289e718a9f4215bb53ed31
GeethapriyanS <geethapriyan.s2023cse@sece.ac.in>
2026-06-15 16:10:56 +0530

    Problem 7 and 8 added

---
+++ Problem8.java
@@ -0,0 +1,9 @@
+public class Problem8 {
+    public static void main(String[] args) {
+        String binaryNumber = "100";
+        int decimalNumber = Integer.parseInt(binaryNumber, 2);
+
+        System.out.println("Input a binary number: " + binaryNumber);
+        System.out.println("Decimal Number: " + decimalNumber);
+    }
+}
